e43a46e0d04822544c312dc568a12b374bc7aeed
aschnall <68933795+aschnall@users.noreply.github.com>
2022-10-05 09:23:58 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,123 @@
+import requests
+import statistics
+import sys
+from bs4 import BeautifulSoup
+import collections
+collections.Callable = collections.abc.Callable
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # parse HTML from link and isolate salaries
+    data_url = 'https://questionnaire-148920.appspot.com/swe/data.html'
+    html_text = requests.get(data_url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    salaries = soup.find_all('td', attrs={'class': 'player-salary'})
+
+    # traverse salaries, clean, convert to ints, and add to list
+    all_salaries = []
+    min_sal = sys.maxsize
+    for salary in salaries:
+        cur_salary = salary.get_text()
+        cur_salary = cur_salary.replace("$", "")
+        cur_salary = cur_salary.replace(",", "")
+        try:
+            cur_salary_int = int(cur_salary)
+            all_salaries.append(cur_salary_int)
+            if cur_salary_int < min_sal:
+                min_sal = cur_salary_int
+        except ValueError:
+            continue
+
+    # sort salaries and get total number of players
+    all_salaries.sort(reverse=True)
+    num_players = len(all_salaries)
+
+    # isolate/calculate top 125 salaries
+    top125_salary_sum = 0
+    top_125_sals = []
+    max_sal = all_salaries[0]
+    for i in range(125):
+        top125_salary_sum += all_salaries[i]
+        top_125_sals.append(all_salaries[i])
+    top125_min = top_125_sals[-1]
+
+    # calculate qualifying offer
+    qualifying_offer = top125_salary_sum / 125
+    qo_rank = 0
+    
+    # isolate/calculate non-min salaries
+    non_min_salary_sum = 0
+    non_min_sals = []
+    for i in range(len(all_salaries)):
+        if all_salaries[i] == min_sal:
+            break
+        non_min_salary_sum += all_salaries[i]
+        non_min_sals.append(all_salaries[i])
+        if all_salaries[i] > qualifying_offer:
+            qo_rank += 1
+    non_min_min = non_min_sals[-1]
+
+    # calculate medians
+    top_125_med = statistics.median(top_125_sals)
+    all_med = statistics.median(all_salaries)
+    non_min_med = statistics.median(non_min_sals)
+
+    # calculate means
+    top_125_mean = statistics.mean(top_125_sals)
+    all_mean = statistics.mean(all_salaries)
+    non_min_mean = statistics.mean(non_min_sals)
+
+    #format numbers for display
+    qualifying_offer_fmt = "${:,.2f}".format(qualifying_offer)
+    qualifying_offer_fmt = qualifying_offer_fmt[0:len(qualifying_offer_fmt)-3]
+    top_125_mean_fmt = "${:,.2f}".format(top_125_mean)
+    top_125_mean_fmt = top_125_mean_fmt[0:len(top_125_mean_fmt)-3]
+    all_mean_fmt = "${:,.2f}".format(all_mean)
+    all_mean_fmt = all_mean_fmt[0:len(all_mean_fmt)-3]
+    top_125_med_fmt = "${:,.2f}".format(top_125_med)
+    top_125_med_fmt = top_125_med_fmt[0:len(top_125_med_fmt)-3]
+    all_med_fmt = "${:,.2f}".format(all_med)
+    all_med_fmt = all_med_fmt[0:len(all_med_fmt)-3]
+    num_players_fmt = "{:,.2f}".format(num_players)
+    num_players_fmt = num_players_fmt[0:len(num_players_fmt)-3]
+    non_min_med_fmt = "${:,.2f}".format(non_min_med)
+    non_min_med_fmt = non_min_med_fmt[0:len(non_min_med_fmt)-3]
+    non_min_mean_fmt = "${:,.2f}".format(non_min_mean)
+    non_min_mean_fmt = non_min_mean_fmt[0:len(non_min_mean_fmt)-3]
+    non_min_players_fmt = "{:,.2f}".format(len(non_min_sals))
+    non_min_players_fmt = non_min_players_fmt[0:len(non_min_players_fmt)-3]
+    non_min_min_fmt = "${:,.2f}".format(non_min_min)
+    non_min_min_fmt = non_min_min_fmt[0:len(non_min_min_fmt)-3]
+    max_sal_fmt = "${:,.2f}".format(max_sal)
+    max_sal_fmt = max_sal_fmt[0:len(max_sal_fmt)-3]
+    top125_min_fmt = "${:,.2f}".format(top125_min)
+    top125_min_fmt = top125_min_fmt[0:len(top125_min_fmt)-3]
+    min_sal_fmt = "${:,.2f}".format(min_sal)
+    min_sal_fmt = min_sal_fmt[0:len(min_sal_fmt)-3]
+
+    # bundling all data into object to use on the front end
+    data = {
+        'qual_off': qualifying_offer_fmt, 
+        'num_players': num_players_fmt,
+        'non_min_players': non_min_players_fmt, 
+        'top_125_med': top_125_med_fmt, 
+        'all_med': all_med_fmt, 
+        'top_125_mean': top_125_mean_fmt,
+        'all_mean': all_mean_fmt, 
+        'non_min_mean': non_min_mean_fmt,
+        'non_min_med': non_min_med_fmt,
+        'all_sals': all_salaries,
+        'top125_sals': top_125_sals,
+        'non_min_sals': non_min_sals,
+        'top_125_max': max_sal_fmt,
+        'top_125_min': top125_min_fmt,
+        'non_min_min': non_min_min_fmt,
+        'non_min_max': max_sal_fmt,
+        'all_max': max_sal_fmt,
+        'all_min': min_sal_fmt,
+        'qo_rank': qo_rank
+    }
+
+    return render_template("index.html", data=data)
---
+++ static/index.js
@@ -0,0 +1,194 @@
+const ctxAll = document.getElementById('histogram-all').getContext('2d');
+const ctxTop125 = document.getElementById('histogram-top125').getContext('2d');
+const ctxNonMin = document.getElementById('histogram-nonmin').getContext('2d');
+const noteElem = document.getElementById('note');
+const notePart1 = 'Note: The Qualifying Offer would\'ve ranked as the ';
+const notePart2 = 'highest salary in the MLB in 2016';
+
+// parse data received from backend received as string - convert to array
+allSalData = allSalData.slice(1,allSalData.length-1)
+allSalDataArr = allSalData.split(',')
+top125SalData = top125SalData.slice(1,top125SalData.length-1)
+top125SalDataArr = top125SalData.split(',')
+nonMinSalData = nonMinSalData.slice(1,nonMinSalData.length-1)
+nonMinSalDataArr = nonMinSalData.split(',')
+
+allSalHist = calculateRanges(allSalDataArr)
+top125SalHist = calculateRanges(top125SalDataArr)
+nonMinSalHist = calculateRanges(nonMinSalDataArr)
+determineRank(qoRank)
+
+// determine the ending for the qual offer rank
+function determineRank(qo_rank) {
+    if (qo_rank % 10 == 1) {
+        noteElem.textContent = notePart1 + qoRank + "st " + notePart2;
+    } else if (qo_rank % 10 == 2) {
+        noteElem.textContent = notePart1 + qoRank + "nd " + notePart2;
+    } else if (qo_rank % 10 == 3) {
+        noteElem.textContent = notePart1 + qoRank + "rd " + notePart2;
+    } else {
+        noteElem.textContent = notePart1 + qoRank + "th " + notePart2;
+    }
+}
+
+// function to calculate salary ranges for histogram
+function calculateRanges(salData) {
+    salHist = Array(8).fill(0)
+    for (let i = 0; i < salData.length; i++) {
+        curSal = parseInt(salData[i])
+        switch (true) {
+            case (curSal >= 1000000 && curSal < 5000000):
+                salHist[1]++
+                break;
+            case (curSal >= 5000000 && curSal < 10000000):
+                salHist[2]++
+                break;
+            case (curSal >= 10000000 && curSal < 15000000):
+                salHist[3]++
+                break;
+            case (curSal >= 15000000 && curSal < 20000000):
+                salHist[4]++
+                break;
+            case (curSal >= 20000000 && curSal < 25000000):
+                salHist[5]++
+                break;
+            case (curSal >= 25000000 && curSal < 30000000):
+                salHist[6]++
+                break;
+            case (curSal >= 30000000):
+                salHist[7]++
+                break;
+            default:
+                salHist[0]++
+                break;
+        }
+    }
+    return salHist
+}
+
+// create the historgrams
+const chartTop125 = new Chart(ctxTop125, {
+    type: 'bar',
+    data: {
+      labels: ['<1M', '1M-5M', '5M-10M', '10M-15M', '15M-20M', '20M-25M', '25M-30M', '30M+'],
+      datasets: [{
+        label: 'Number of Players in Salary Range',
+        data: top125SalHist,
+        backgroundColor: 'grey',
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        title: {
+        display: true,
+        text: 'Histogram of Top 125 Player Salaries'
+      },
+      },
+      scales: {
+        xAxes: [{
+          display: false,
+          barPercentage: 1.3,
+          ticks: {
+            max: 3,
+          }
+        }, {
+          display: true,
+          ticks: {
+            autoSkip: false,
+            max: 4,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+
+const chartAll = new Chart(ctxAll, {
+  type: 'bar',
+  data: {
+    labels: ['<1M', '1M-5M', '5M-10M', '10M-15M', '15M-20M', '20M-25M', '25M-30M', '30M+'],
+    datasets: [{
+      label: 'Number of Players in Salary Range',
+      data: allSalHist,
+      backgroundColor: 'grey',
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+      display: true,
+      text: 'Histogram of All Player Salaries'
+    },
+    },
+    scales: {
+      xAxes: [{
+        display: false,
+        barPercentage: 1.3,
+        ticks: {
+          max: 3,
+        }
+      }, {
+        display: true,
+        ticks: {
+          autoSkip: false,
+          max: 4,
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+const chartNonMin = new Chart(ctxNonMin, {
+    type: 'bar',
+    data: {
+      labels: ['<1M', '1M-5M', '5M-10M', '10M-15M', '15M-20M', '20M-25M', '25M-30M', '30M+'],
+      datasets: [{
+        label: 'Number of Players in Salary Range',
+        data: nonMinSalHist,
+        backgroundColor: 'grey',
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        title: {
+        display: true,
+        text: 'Histogram of Non-Minimum Player Salaries'
+      },
+      },
+      scales: {
+        xAxes: [{
+          display: false,
+          barPercentage: 1.3,
+          ticks: {
+            max: 3,
+          }
+        }, {
+          display: true,
+          ticks: {
+            autoSkip: false,
+            max: 4,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
